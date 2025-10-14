109ecdcb6b82540a703403f4a7b36078a1a72d5e
CHANG <jc.jonathan.chang94@gmail.com>
2025-10-14 13:14:42 +0200

    Déploiement automatique 11

--- src/app.js
+++ src/app.js
@@ -14,3 +14,14 @@ function citationAleatoire() {
 
 // Export du module
 module.exports = citationAleatoire;
+
+export default function citationAleatoire() {
+  const citations = [
+    "Le succès est la somme de petits efforts répétés jour après jour.",
+    "Apprendre sans réfléchir est vain. Réfléchir sans apprendre est dangereux.",
+    "La simplicité est la sophistication suprême.",
+    "La meilleure façon de prédire l’avenir, c’est de le créer."
+  ];
+  const index = Math.floor(Math.random() * citations.length);
+  return citations[index];
+}
