e0acad4edfc93e15471db0aa57cf082dd61504e6
CHANG <jc.jonathan.chang94@gmail.com>
2025-10-14 14:04:19 +0200

    Déploiement automatique 15

--- src/app.js
+++ src/app.js
@@ -1,16 +1,13 @@
-// src/app.js
+const citations = [
+  "Le succès est la somme de petits efforts répétés jour après jour.",
+  "Apprendre sans réfléchir est vain. Réfléchir sans apprendre est dangereux.",
+  "La simplicité est la sophistication suprême.",
+  "La meilleure façon de prédire l’avenir, c’est de le créer."
+];
+
 function citationAleatoire() {
-  const citations = [
-    "Le succès est la somme de petits efforts répétés jour après jour.",
-    "Apprendre sans réfléchir est vain. Réfléchir sans apprendre est dangereux.",
-    "La simplicité est la sophistication suprême.",
-    "La meilleure façon de prédire l’avenir, c’est de le créer."
-  ];
   const index = Math.floor(Math.random() * citations.length);
   return citations[index];
 }
 
-// Pour Node.js / Jest
 module.exports = citationAleatoire;
-
-// Pour navigateur, on créera app-browser.js
